96644c49726dc95adcf70029653e821c7335fbd7
Pluto Yang <yangyj.ee@gmail.com>
2018-06-08 14:29:46 +0800

    Add a carousel in home page

--- home/models.py
+++ home/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 
-from wagtail.core.models import Page
-
+from modelcluster.fields import ParentalKey
+from wagtail.core.models import Page, Orderable
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 class HomePage(Page):
-    pass
+    content_panels = Page.content_panels + [
+        InlinePanel('gallery_images', label="首页图片"),
+    ]
 
     class Meta:
         verbose_name = "首页"
+
+class HomePageGallery(Orderable):
+    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+    url = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
+        FieldPanel('url'),
+    ]
