13fc0f2d15009358ebdb95f243afc57ebe95c3c6
Pluto Yang <yangyj.ee@gmail.com>
2018-06-09 18:14:45 +0800

    Add 10 most recent news to the homepage

--- home/models.py
+++ home/models.py
@@ -5,10 +5,17 @@ from wagtail.core.models import Page, Orderable
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 
+from news.models import NewsPage
+
 class HomePage(Page):
     content_panels = Page.content_panels + [
         InlinePanel('gallery_images', label="首页图片"),
     ]
+
+    def get_context(self, request):
+        context = super(HomePage, self).get_context(request)
+        context['newspage'] = NewsPage.objects.live().order_by("-date")[:10]
+        return context
 
     class Meta:
         verbose_name = "首页"
